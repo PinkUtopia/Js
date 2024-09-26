105cc409db42ca8663983602b2ccfd0ddc55c41d
PinkUtopia <pratikdeshmukh7477@gmail.com>
2024-09-26 14:13:49 +0000

    container config done

---
+++ .devcontainer/01_Basics/variables.js
@@ -0,0 +1,24 @@
+const accountId=13443
+let accountEmail="pratikdeshmukh7477@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+//accountId="3" cant change the value of constant 
+accountEmail="pratikdeshmukh6977@gmail.com"
+accountPassword="5678"
+accountCity="Jaisalmer"
+
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.log(accountEmail,accountId,accountPassword,accountCity)
+
+
+
+//see above everything except constant can be changed
+
+
+
