2346f52d5567afede253f5b12e13ac9c5bf2159f
PinkUtopia <pratikdeshmukh7477@gmail.com>
2024-09-27 05:45:51 +0000

    container config done

--- .devcontainer/01_Basics/datatypes.js
+++ .devcontainer/01_Basics/datatypes.js
@@ -1,1 +1,20 @@
 console.log("hello");
+"use strict"; //treats js as new version of js 
+
+// alert(3+3) we are using node.js not browser so this wont work 
+
+
+let name="Pratik" //string 
+let age=18
+let isLoggedIn=true //boolean  true or false
+let state=null
+
+// number is in range 2^53-1 highest 
+// for bigger number use bigint
+
+//null is actually a standalone value/type
+//there is also undefined means not defined yet   null means no value
+// symbol used to make something unique 
+//object 
+
+console.log(typeof "pratik")
