68bab489d42934ac5414314ae2fc90533bf5bdef
PinkUtopia <pratikdeshmukh7477@gmail.com>
2024-09-27 05:23:26 +0000

    container config done

--- .devcontainer/01_Basics/variables.js
+++ .devcontainer/01_Basics/variables.js
@@ -2,22 +2,24 @@ const accountId=13443
 let accountEmail="pratikdeshmukh7477@gmail.com"
 var accountPassword="12345"
 accountCity="Jaipur"
+let accountName; //you can define variables like this without actually deining a value 
 
 //accountId="3" cant change the value of constant 
 accountEmail="pratikdeshmukh6977@gmail.com"
 accountPassword="5678"
-accountCity="Jaisalmer"
+accountCity="Jaisalmer" //still a varibale but not favourable method 
 
 
 console.log(accountId);
 console.log(accountEmail);
 console.log(accountPassword);
 console.log(accountCity);
+console.log(accountName);
+/*
+try not to use var use let beacuse of scope in for and if else {}
+*/
 
 console.log(accountEmail,accountId,accountPassword,accountCity)
-
-
-
 //see above everything except constant can be changed
 
 
